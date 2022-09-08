6ffaafd349c9fa061f601bf46474fd07efeb3c3e
Norbert Nopper <nopper@ux3d.io>
2022-09-08 16:30:31 +0200

    Updated to Blender 3.3

--- 2gltf2.bat
+++ 2gltf2.bat
@@ -2,7 +2,7 @@ ECHO OFF
 
 IF "%1"=="" GOTO USAGE
 
-"C:/Program Files/Blender Foundation/Blender 3.1/blender.exe" -b -P 2gltf2.py -- %1
+"C:/Program Files/Blender Foundation/Blender 3.3/blender.exe" -b -P 2gltf2.py -- %1
 GOTO END
 
 :USAGE
