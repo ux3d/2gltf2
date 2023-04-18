de35c747ae904ca9eb5079326f2e9441c66d5fe4
Norbert Nopper <nopper@ux3d.io>
2023-04-18 14:23:07 +0200

    Added possibility to execute the linux shell script from another path.

--- 2gltf2.sh
+++ 2gltf2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ "$#" = 1 ]
 then
-	blender -b -P 2gltf2.py -- "$1"
+	blender -b -P $PWD/2gltf2.py -- "$1"
 else
 	echo To glTF 2.0 converter.
 	echo Supported file formats: .abc .blend .dae .fbx. .obj .ply .stl .usd .wrl .x3d
